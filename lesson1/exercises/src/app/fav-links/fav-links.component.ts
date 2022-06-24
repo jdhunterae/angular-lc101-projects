209863d11165e9ae2c710aa7bb629060d4424bad
Andrew Pomerleau <arpomerleau@gmail.com>
2022-06-23 21:25:23 -0400

    updated fav-links component html

--- lesson1/exercises/src/app/fav-links/fav-links.component.ts
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fav-links',
+  selector: 'fav-links',
   templateUrl: './fav-links.component.html',
   styleUrls: ['./fav-links.component.css']
 })
 export class FavLinksComponent implements OnInit {
+  favLinks = ['https://www.google.com', 
+  'http://www.github.com',
+   'http://www.youtube.com'];
+   title = "Favorite Links";
 
   constructor() { }
 
